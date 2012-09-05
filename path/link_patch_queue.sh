d1d817226cd68a28c6788800b209787fbad7bdff
Benoit Girard <b56girard@gmail.com>
2012-09-04 23:52:27 -0400

    Remove . from hostnames

--- path/link_patch_queue.sh
+++ path/link_patch_queue.sh
@@ -1,7 +1,7 @@
 cp -r .hg/patches-$(hg qqueue --active) ~/Dropbox/patches/ && {
   echo "Copied patches to dropbox"
-  ln -s ~/Dropbox/patches/patches-$(hg qqueue --active) .hg/patches-$(hg qqueue --active)-$HOSTNAME && {
+  ln -s ~/Dropbox/patches/patches-$(hg qqueue --active) .hg/patches-$(hg qqueue --active)-${HOSTNAME%.*} && {
     echo "Link created"
-    echo $(hg qqueue --active)-$HOSTNAME >> .hg/patches.queues
+    echo $(hg qqueue --active)-${HOSTNAME%.*} >> .hg/patches.queues
   }
 }
