e9cf028c8928600377a5299a30a6af884dd8d607
Benoit Girard <bgirard@mozilla.com>
2013-03-25 15:03:08 -0400

    Use j8

--- path/incremental_build.sh
+++ path/incremental_build.sh
@@ -6,10 +6,10 @@ echo "Build: $(cat ~/.config/moz_tree)"
 for i in $*
 do
   echo "Building $i"
-  echo make -C $(cat ~/.config/moz_tree)/$i
-  make -C $(cat ~/.config/moz_tree)/$i #> /dev/null
+  echo make -j8 -C $(cat ~/.config/moz_tree)/$i
+  make -j8 -C $(cat ~/.config/moz_tree)/$i #> /dev/null
 done
 
 echo "Linking"
-make -C $(cat ~/.config/moz_tree)/toolkit/library > /dev/null
+make -j8 -C $(cat ~/.config/moz_tree)/toolkit/library > /dev/null
 
