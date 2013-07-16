cd1542a8c7cbe85d0842aedc3e2a138dc299652a
Benoit Girard <bgirard@mozilla.com>
2013-07-16 17:07:55 -0400

    Shortcut for running gtest

--- path/incremental_build.sh
+++ path/incremental_build.sh
@@ -11,11 +11,19 @@ fi
 
 for i in $*
 do
+  if test "$i" = "gtest"; then
+    RUN_GTEST=1
+    continue;
+  fi
   echo "Building $i"
   echo make -j8 -C $(cat .config/moz_tree)/$i
   # Build using a single core for diagnostics
   make -C $(cat .config/moz_tree)/$i #> /dev/null
 done
 
+if test "$RUN_GTEST" = "1"; then
+  mach.sh gtest
+  exit
+fi
 echo "Linking"
 make -C $(cat .config/moz_tree)/toolkit/library #> /dev/null
